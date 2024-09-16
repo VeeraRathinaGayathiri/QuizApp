239253964ab0f5564a91ce0e8bf3aa14a8ea5b26
VeeraRathinaGayathiri <gayathrisundar1206@gmail.com>
2024-09-16 19:19:36 +0100

    initial push

---
+++ quizApp/src/test/java/com/practice/quizApp/QuizAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.quizApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
